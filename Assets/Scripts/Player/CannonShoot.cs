1c5eb6daac1ca3e99a0b74e8d4e7a1c505592763
ZyronMelancon <altausolaris@gmail.com>
2017-10-25 17:06:51 -0500

    fixed up shoot script, added particle system to bullet, adjusted speed, added destroy over time

--- Assets/Scripts/Player/CannonShoot.cs
+++ Assets/Scripts/Player/CannonShoot.cs
@@ -5,19 +5,29 @@ using UnityEngine;
 public class CannonShoot : MonoBehaviour
 {
     public GameObject bulletPrefab;
-    private float bulletSpeed = 1;
+    public float bulletSpeed = 20;
+    public float bulletDestroyTime = 5;
 
     void OnShoot()
     {
         GameObject Clone;
 
         Clone = (Instantiate(bulletPrefab, transform.position, transform.rotation)) as GameObject;
+        Clone.GetComponent<Transform>().parent = null;
+        Clone.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector3(0, bulletSpeed, 0));
         Debug.Log("Bullet has been found");
+        StartCoroutine(DestroyBullet(Clone));
     }
 
 	void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.Space));
+        if (Input.GetKeyDown(KeyCode.Space))
             OnShoot();
 	}
+
+    IEnumerator DestroyBullet(Object bullet)
+    {
+        yield return new WaitForSecondsRealtime(bulletDestroyTime);
+        Destroy(bullet);
+    }
 }
