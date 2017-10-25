866f8918e1949331a48bc7c3f086590dd1e3c5ea
ZyronMelancon <altausolaris@gmail.com>
2017-10-25 17:53:16 -0500

    working on trail particles

---
+++ Assets/Scripts/BulletParticleSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletParticleSystem : MonoBehaviour {
+
+    public ParticleSystem trail;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnDestroy()
+    {
+        trail.transform.parent = null;
+    }
+}
