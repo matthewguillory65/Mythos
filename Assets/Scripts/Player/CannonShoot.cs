d2e0583877a9ad05d6f9bfd7fde36c9627a5cb11
matthewguillory65 <s178062@students.aie.edu.au>
2017-10-25 16:27:27 -0500

    Work for 13:30 - 16:30
    Making the Cannonball Shoot attempt, as well as making the spawnpoint
    and the cannonball

---
+++ Assets/Scripts/Player/CannonShoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonShoot : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    private float bulletSpeed = 1;
+
+    void OnShoot()
+    {
+        GameObject Clone;
+
+        Clone = (Instantiate(bulletPrefab, transform.position, transform.rotation)) as GameObject;
+        Debug.Log("Bullet has been found");
+    }
+
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space));
+            OnShoot();
+	}
+}
