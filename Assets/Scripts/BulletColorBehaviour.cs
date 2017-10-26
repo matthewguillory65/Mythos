92cd26e8af71a62be36c30dd8da5b75fbe461f21
ZyronMelancon <altausolaris@gmail.com>
2017-10-26 15:04:52 -0500

    hella aesthetics

--- Assets/Scripts/BulletColorBehaviour.cs
+++ Assets/Scripts/BulletColorBehaviour.cs
@@ -5,21 +5,59 @@ using UnityEngine;
 public class BulletColorBehaviour : MonoBehaviour {
 
     public int colorType;
+    public GameObject bullet;
+    public ParticleSystem trailParticle;
+    public ParticleSystem explode;
+    public GameObject pointLight;
 
     private Collider2D thisCollider;
-    public GameObject bullet;
-    public ParticleSystem explode;
+
+    void setColor()
+    {
+        colorType = Random.RandomRange(0, 3);
+
+        switch(colorType)
+        {
+            case 0:
+                pointLight.GetComponent<Light>().color = new Color(1, 0.4f, 0.6f);
+                explode.startColor = new Color(1, 0.4f, 0.6f);
+                trailParticle.startColor = new Color(1, 0.4f, 0.6f);
+                break;
+            case 1:
+                pointLight.GetComponent<Light>().color = new Color(0.3f, 1, 0.7f);
+                explode.startColor = new Color(0.3f, 1, 0.7f);
+                trailParticle.startColor = new Color(0.3f, 1, 0.7f);
+                break;
+            case 2:
+                pointLight.GetComponent<Light>().color = new Color(0.3f, 0.6f, 1);
+                explode.startColor = new Color(0.3f, 0.8f, 1);
+                trailParticle.startColor = new Color(0.3f, 0.8f, 1);
+                break;
+        }
+    }
 
 	// Use this for initialization
 	void Start ()
     {
         thisCollider = GetComponentInChildren<Collider2D>();
+        setColor();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void explodeBullet()
+    {
+        gameObject.GetComponent<BulletParticleSystem>().trail.emissionRate = 0;
+        Destroy(bullet);
+        Destroy(gameObject, 1);
+        explode.transform.parent = null;
+        explode.Play();
+        Destroy(explode.gameObject, 1);
+        Destroy(pointLight);
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -29,12 +67,11 @@ public class BulletColorBehaviour : MonoBehaviour {
             if (collision.gameObject.GetComponent<EnemyColorBehaviour>().colorType == colorType)
             {
                 Destroy(collision.gameObject);
-                gameObject.GetComponent<BulletParticleSystem>().trail.emissionRate = 0;
-                Destroy(bullet);
-                Destroy(gameObject, 1);
-                explode.transform.parent = null;
-                explode.Play();
-                Destroy(explode.gameObject, 1);
+                explodeBullet();
+            }
+        if (collision.gameObject.tag == "Back")
+            {
+                explodeBullet();
             }
     }
 }
