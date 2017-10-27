c1b98b7618f29df4c580579de5b8b1c406e9157b
matthewguillory65 <matthewguillory65@gmail.com>
2017-10-27 12:38:22 -0500

    UI
    Work on UI and score and health

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Slider health;
+    public GameObject Wall;
+    public GameObject Enemy;
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+    public Text score;
+    private Rigidbody rb;
+    public GameObject enemy;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void EnemyDestroyed(Collider other)
+    {
+        if(other.gameObject.CompareTag("Enemy Dead"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        score.text = "Score:" + score.ToString();
+        if(count >= 999)
+        {
+            score.text = "You win!";
+        }
+    }
+
+	// Update is called once per frame
+	void Update()
+    {
+		
+	}
+}
